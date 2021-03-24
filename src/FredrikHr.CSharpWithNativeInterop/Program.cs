1c2c7b0b54d268b2222619f40f423bca8c2a5b9f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-03-24 09:49:18 +0100

    Create a new C# console project

---
+++ src/FredrikHr.CSharpWithNativeInterop/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FredrikHr.CSharpWithNativeInterop
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
